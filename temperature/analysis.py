aad8cd12d85dd2a1f387948dcb67990cfd7400ad
Eric Lee <saintgimp@hotmail.com>
2013-10-25 22:02:17 -0700

    Tweaks to data collection and analysis

--- temperature/analysis.py
+++ temperature/analysis.py
@@ -1,12 +1,12 @@
 import datetime, numpy, scipy, pandas
 import matplotlib.pyplot as plt
 
-df = pandas.read_table("temperature_data.txt", sep=",", header=None, names=["timestamp", "temp"], index_col=0, parse_dates=True)
+df = pandas.read_table("/Users/elee/temperature_data.txt", sep=",", header=0, index_col=0, parse_dates=True)
 
 print df.columns
 print df["temp"]
 
 
 minutes = df.temp.resample("1T", how="mean")
-minutes.plot(kind="line")
+df.temp.plot(kind="line")
 plt.show()
