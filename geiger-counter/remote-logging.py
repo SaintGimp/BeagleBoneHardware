93a2b511690369f850a3edbf99f10775d0b843c3
Eric Lee <saintgimp@hotmail.com>
2014-08-13 16:40:58 -0700

    Log remote geiger data once per minute

--- geiger-counter/remote-logging.py
+++ geiger-counter/remote-logging.py
@@ -6,6 +6,7 @@ import urllib
 import threading
 import queue
 import sys
+import datetime
 
 queue = queue.Queue()
 
@@ -26,30 +27,40 @@ headers = {
 def sendData():
 	while True:
 		body = queue.get()
-		phantServer.request(method="POST", url="/input/BD8mwo8ZMdU73kZP7MXAcK7oaJR", body=body, headers=headers)
+		phantServer.request(method="POST", url="/input/3K2oGxKqelCo0ry50Ln4fpr1q7m", body=body, headers=headers)
 		response = phantServer.getresponse()
 		response.read()
 
-		if response.status != 200:
+		# TODO: retry on transient errors
+		if response.status == 200:
+			print("Logged to server: " + body)
+		else:
 			print("Phant server returned status " + str(response.status) + ": " + response.reason)
 
 thread = threading.Thread(target = sendData)
 thread.daemon = True
 thread.start()
 
+number_of_updates = 0;
+
 while True:
 	bytes = ser.read(36)
 
 	if len(bytes) == 36:
+		number_of_updates += 1
+
 		line1 = bytes[2:18].decode('ascii')
 		line2 = bytes[20:36].decode('ascii')
-		print(line1)
-		print(line2)
+		print(format(number_of_updates, '02d') + "| " + line1 + "  " + line2)
 
-		cpm = int(re.search(r'CPM:\s*(\d+)', line1).group(1))
-		cps = int(re.search(r'CPS:\s*(\d+)', line1).group(1))
-		usph = float(re.search(r'uSv/hr:\s*(\d+\.\d+)', line2).group(1))
+		# TODO: this relies on the geiger counter being fairly precise in its update
+		# interval. We'll probably want to calculate minute intervals in this script
+		# instead.
+		if number_of_updates == 60:
+			cpm = int(re.search(r'CPM:\s*(\d+)', line1).group(1))
 
-		body = urllib.parse.urlencode({'CPS': cps, 'CPM': cpm, 'uSv_Hr': usph})
-		queue.put_nowait(body)
+			body = urllib.parse.urlencode({'CPM': cpm, 'Device_Time': str(datetime.datetime.now())})
+			queue.put_nowait(body)
+
+			number_of_updates = 0
 
