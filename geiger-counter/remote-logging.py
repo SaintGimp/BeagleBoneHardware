e85b249686c418b01ade1452a8dde02acb8040bd
Eric Lee <saintgimp@hotmail.com>
2014-08-14 17:17:17 -0700

    Poll geiger data at precise one-minute intervals, and handle errors

--- geiger-counter/remote-logging.py
+++ geiger-counter/remote-logging.py
@@ -7,6 +7,8 @@ import threading
 import queue
 import sys
 import datetime
+import time
+import socket
 
 queue = queue.Queue()
 
@@ -17,7 +19,6 @@ uart.open()
 # the middle of a serial message or if a byte is dropped for any
 # reason, we'll throw away the partial message and try again
 ser = serial.Serial(port = "/dev/ttyO1", baudrate=9600, timeout=0.25) 
-phantServer = http.client.HTTPConnection("phant.saintgimp.org")
 
 headers = {
 	"Phant-Private-Key": str(sys.argv[1]),
@@ -27,40 +28,55 @@ headers = {
 def sendData():
 	while True:
 		body = queue.get()
-		phantServer.request(method="POST", url="/input/3K2oGxKqelCo0ry50Ln4fpr1q7m", body=body, headers=headers)
-		response = phantServer.getresponse()
-		response.read()
 
-		# TODO: retry on transient errors
-		if response.status == 200:
-			print("Logged to server: " + body)
-		else:
-			print("Phant server returned status " + str(response.status) + ": " + response.reason)
+		success = False
+		while not success:
+			try:
+				phantServer = http.client.HTTPConnection("phant.saintgimp.org")
+				phantServer.request(method="POST", url="/input/3K2oGxKqelCo0ry50Ln4fpr1q7m", body=body, headers=headers)
+				response = phantServer.getresponse()
+				response.read()
 
-thread = threading.Thread(target = sendData)
-thread.daemon = True
-thread.start()
+				if response.status == 200:
+					success = True
+					print("Logged to server: " + body)
+				else:
+					print("Phant server returned status " + str(response.status) + ": " + response.reason)
+			except (http.client.HTTPException, socket.error) as err:
+				print("HTTP error: {0}".format(err))
 
-number_of_updates = 0;
+			if not success:
+				time.sleep(5)
+				print("Retrying...")
+
+def oncePerMinute():
+	global next_interval_time
+	while True:
+		# Sleep for the remainder of the time until the next
+		# interval, prevents timer drift
+		next_interval_time += 60
+		sleep_time = next_interval_time - time.time()
+		time.sleep(sleep_time)
+
+		body = urllib.parse.urlencode({'CPM': cpm, 'Device_Time': str(datetime.datetime.now())})
+		queue.put_nowait(body)
+
+sendThread = threading.Thread(target = sendData)
+sendThread.daemon = True
+sendThread.start()
+
+next_interval_time = time.time()
+
+sampleThread = threading.Thread(target = oncePerMinute)
+sampleThread.daemon = True
+sampleThread.start()
 
 while True:
 	bytes = ser.read(36)
 
 	if len(bytes) == 36:
-		number_of_updates += 1
-
 		line1 = bytes[2:18].decode('ascii')
 		line2 = bytes[20:36].decode('ascii')
-		print(format(number_of_updates, '02d') + "| " + line1 + "  " + line2)
+		print(line1 + "  " + line2)
 
-		# TODO: this relies on the geiger counter being fairly precise in its update
-		# interval. We'll probably want to calculate minute intervals in this script
-		# instead.
-		if number_of_updates == 60:
-			cpm = int(re.search(r'CPM:\s*(\d+)', line1).group(1))
-
-			body = urllib.parse.urlencode({'CPM': cpm, 'Device_Time': str(datetime.datetime.now())})
-			queue.put_nowait(body)
-
-			number_of_updates = 0
-
+		cpm = int(re.search(r'CPM:\s*(\d+)', line1).group(1))
