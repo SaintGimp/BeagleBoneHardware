a694be299c9b0bf514613b94d4d60b7fc2a12c60
Eric Lee <saintgimp@hotmail.com>
2015-04-17 20:17:34 -0700

    Alert when Phant request fails

--- geiger-counter/watchdog/run.js
+++ geiger-counter/watchdog/run.js
@@ -2,6 +2,8 @@ var request = require('request');
 var Twitter = require('twitter');
 
 function notify() {
+  console.log("Hey, I think the geiger counter is offline!");
+
   var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -9,9 +11,12 @@ function notify() {
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
   });
 
-  client.post('statuses/update', {status: 'Hey @saintgimp, I think the geiger counter is offline!'},  function(error, tweet, response){
-    if (error) throw error;
-  });  
+  client.post('direct_messages/new', {screen_name: 'saintgimp', text: 'Hey, I think the geiger counter is offline!'},  function(error, tweet, response){
+    if (error) {
+      console.log(response);
+      throw error;
+    }
+  });
 }
 
 var options = {
@@ -21,20 +26,17 @@ var options = {
 console.log(options.url);
 
 request(options, function (error, response, body) {
-  if (error) throw error;
-  
   if (response.statusCode == 200) {
     var latestEntry = new Date(body[0].timestamp);
     var now = new Date();
     var elapsedMinutes = (now.getTime() - latestEntry.getTime()) / 1000 / 60;
     if (elapsedMinutes > 30) {
-      console.log("Hey, I think the geiger counter is offline!");
-    	notify();
+      notify();
     } else {
-      console.log("Everything's fine here, we're all fine, how are you?")
+      console.log("Everything's fine here, we're all fine, how are you?");
     }
   } else {
-    console.log(error);
-    console.log(response);
+    console.log("Hey, I think the geiger counter is offline!");
+    notify();
   }
 });
